e923e2586dfa5b22a6d039e989ef19080f9fedce
Robert <robbtish@gmail.com>
2024-06-20 16:42:03 +0300

    Delete eda changed path for parsing add 3 csv files for some rooms

--- src/parse_cian.py
+++ src/parse_cian.py
@@ -5,14 +5,15 @@ import datetime
 import cianparser
 import pandas as pd
 
+
 moscow_parser = cianparser.CianParser(location="Москва")
 
 
 def main():
     """Function docstring"""
     t = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-    n_rooms = 1
-    csv_path = f'data/raw/{n_rooms}_{t}.csv'
+    n_rooms = 3
+    csv_path = f'E:/pabd/data/raw/{n_rooms}_{t}.csv'
     data = moscow_parser.get_flats(
         deal_type="sale",
         rooms=(n_rooms,),
